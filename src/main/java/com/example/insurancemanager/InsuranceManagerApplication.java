0747eb6cfa406531f18e859c526c07489431d5e2
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-06-19 14:50:47 +0300

    first commit

---
+++ src/main/java/com/example/insurancemanager/InsuranceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/insurancemanager/InsuranceAppVskSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAppVskSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
