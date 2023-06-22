e374af11087eca88ec7848ac94f656970e0fb83d
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-06-22 16:59:01 +0300

    1.0.5 updated PolicyController

--- src/main/java/com/insurancemanager/controller/PolicyController.java
+++ src/main/java/com/insurancemanager/controller/PolicyController.java
@@ -1,11 +1,12 @@
 package com.insurancemanager.controller;
 
+import com.insurancemanager.model.Policy;
+import com.insurancemanager.model.PolicyType;
 import com.insurancemanager.service.PolicyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/policies")
@@ -13,10 +14,48 @@ public class PolicyController {
     @Autowired
     PolicyService policyService;
 
-
-    @GetMapping("/all")
+    @GetMapping("/findall")
     public String findAll(Model model) {
         model.addAttribute("policies", policyService.getAllPolicies());
         return "policies";
     }
+
+    @GetMapping("/create")
+    public String createPolicyForm(Model model) {
+        Policy policy = new Policy();
+        model.addAttribute(policy);
+        model.addAttribute("types", PolicyType.values());
+        return "create_policy";
+    }
+
+    @PostMapping("/")
+    public String savePolicy(@ModelAttribute("policy") Policy policy) {
+        policyService.savePolicy(policy);
+        return "redirect:/policies";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editPolicyForm(@PathVariable Long id, Model model) {
+        model.addAttribute("policy", policyService.getPolicyById(id));
+        return "edit_policy";
+    }
+
+    @PostMapping("/{id}")
+    public String updatePolicy(@PathVariable Long id,
+                               @ModelAttribute("policy") Policy policy,
+                               Model model) {
+        Policy dbPolicy = policyService.getPolicyById(id);
+        dbPolicy.setId(policy.getId());
+        dbPolicy.setNumber(policy.getNumber());
+        ///...
+        policyService.updatePolicy(dbPolicy);
+        return "redirect:/policies";
+    }
+
+    @GetMapping("/{id}")
+    public String deletePolicy(@PathVariable Long id) {
+        policyService.deletePolicy(id);
+        return "redirect:/policies";
+    }
+
 }
