697209148d6aad02ce9a3b543dd8ac4abe8d69bf
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-06-23 16:28:00 +0300

    1.0.6 added CRUD to Agent

---
+++ src/main/java/com/insurancemanager/controller/AgentController.java
@@ -0,0 +1,58 @@
+package com.insurancemanager.controller;
+
+import com.insurancemanager.model.Agent;
+import com.insurancemanager.service.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/agents")
+public class AgentController {
+    @Autowired
+    private AgentService agentService;
+
+    @GetMapping("/add")
+    public String showAgentForm(Model model) {
+        model.addAttribute("agent", new Agent());
+        return "create_agent";
+    }
+
+    @PostMapping("/save")
+    public String saveAgent(@ModelAttribute("agent") Agent agent) {
+        agentService.saveAgent(agent);
+        return "redirect:/agents/findall";
+    }
+
+    @GetMapping("/findall")
+    public String getAllAgents(Model model) {
+        model.addAttribute("agents", agentService.getAllAgents());
+        return "all_agents";
+    }
+
+    @GetMapping("{id}/update")
+    public String showUpdateAgentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("agent", agentService.getAgentById(id));
+        return "edit_agent";
+    }
+
+    @PostMapping("{id}/update")
+    public String showUpdateAgentForm(@PathVariable Long id, @ModelAttribute("agent") Agent updatedAgent) {
+        Agent agent = agentService.getAgentById(id);
+
+        agent.setFirstName(updatedAgent.getFirstName());
+        agent.setLastName(updatedAgent.getLastName());
+        agent.setEmail(updatedAgent.getEmail());
+        agent.setPhoneNumber(updatedAgent.getPhoneNumber());
+
+        agentService.saveAgent(agent);
+        return "redirect:/agents/findall";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteAgent(@PathVariable Long id) {
+        agentService.deleteAgent(id);
+        return "redirect:/agents/findall";
+    }
+}
--- src/main/java/com/insurancemanager/model/Agent.java
+++ src/main/java/com/insurancemanager/model/Agent.java
@@ -1,6 +1,7 @@
 package com.insurancemanager.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "agents")
 public class Agent {
